653410d3db53a99cee53092ff9d36b12ea5f601b
hyonchuloh <hyonchuloh@218.38.137.27>
2021-06-27 00:07:38 +0900

    TEST

---
+++ api-test/src/test/java/com/ohhyonchul/apitest/ApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohhyonchul.apitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
