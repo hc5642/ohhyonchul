aa73e5d6b3d18f47ee9fc66aa2f8843db8406128
hyonchuloh <hc5642@me.com>
2021-06-30 07:10:39 +0900

    POI대체 csv파일 생성테스

--- api-test/src/main/java/com/ohhyonchul/apitest/svc/ApiTestPoiSvcImpl.java
+++ api-test/src/main/java/com/ohhyonchul/apitest/svc/ApiTestPoiSvcImpl.java
@@ -1,40 +1,30 @@
 package com.ohhyonchul.apitest.svc;
 
-import java.io.IOException;
-
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.Font;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class ApiTestPoiSvcImpl implements ApiTestPoiSvc {
 	
 	@Override
 	public String doService() {
-		Workbook workbook = new HSSFWorkbook();
-		Sheet sheet = workbook.createSheet("FEP LINE INFO");
 		
-		Row row = null;
-		Cell cell = null;
-		int rowNo = 0;
-		
-		Font font = workbook.createFont();
-		font.setFontName("d2coding");
-		
-		CellStyle headStyle = workbook.createCellStyle();
-		headStyle.setFont(font);
-		row = sheet.createRow(rowNo++);
-		cell = row.createCell(0);
-		cell.setCellStyle(headStyle);
-		cell.setCellValue("NO");
-		
+		BufferedWriter bw = null;
+		FileWriter fw = null;
 		try {
-			workbook.close();
-		} catch (IOException e) {
+			fw = new FileWriter("test.csv");
+			bw = new BufferedWriter(fw);
+			
+			fw.write("test, test, test, \"test\ntest\"");
+			fw.flush();
+		} catch ( Exception e ) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if ( fw != null ) fw.close();
+				if ( bw != null ) bw.close();
+			} catch  ( Exception e1 ) {
+				e1.printStackTrace();
+			}
 		}
 		
 		return null;
